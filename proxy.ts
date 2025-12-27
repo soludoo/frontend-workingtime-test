a37547e3395f3c644afc190587bf5c70a90f537e
zuandabaransyahputra <zuandabaransyahputra@gmail.com>
2025-12-27 17:21:12 +0700

    [UPDATE] - admin page

--- proxy.ts
+++ proxy.ts
@@ -4,14 +4,14 @@ import type { NextRequest } from "next/server";
 const AUTH_PAGES = ["/login", "/register", "/forgot-password"];
 
 export function proxy(req: NextRequest) {
-  // const { pathname } = req.nextUrl;
+  const { pathname } = req.nextUrl;
   // const token = req.cookies.get("token_working_app")?.value;
 
-  // if (pathname === "/admin") {
-  //   const url = req.nextUrl.clone();
-  //   url.pathname = "/admin/dashboard";
-  //   return NextResponse.redirect(url);
-  // }
+  if (pathname === "/admin") {
+    const url = req.nextUrl.clone();
+    url.pathname = "/admin/dashboard";
+    return NextResponse.redirect(url);
+  }
 
   // const isAuthPage = AUTH_PAGES.some((p) => pathname.startsWith(p));
 
