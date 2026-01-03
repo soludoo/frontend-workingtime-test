45b24097b9f677c42ef321da2ffc308dce518ac0
zuandabaransyahputra <zuandabaransyahputra@gmail.com>
2026-01-03 12:41:56 +0700

    [UPDATE] - changes

--- app/(with-footer)/(homepage)/page.tsx
+++ app/(with-footer)/(homepage)/page.tsx
@@ -1,13 +1,8 @@
 import Content from "./_components/content";
 
 const Page = async () => {
-  const res = await fetch(`${process.env.APP_URL}/api/timer/current`, {
-    next: {
-      revalidate: 60,
-    },
-  });
-  const { data } = await res.json();
-  console.log(data);
+  // const res = await fetch("/api/timer/current");
+  // const { data } = await res.json();
 
   return (
     <section className="pt-5 pb-15 h-full px-5">
